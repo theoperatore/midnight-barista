5881d2e99cbb15e2bfdcb34b70838a6c00412c4b
Alex Petersen <theoperatore@gmail.com>
2019-03-09 16:47:36 -0500

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+  [SerializeField] int minx = -2;
+  [SerializeField] int miny = 0;
+  [SerializeField] int maxx = 0;
+  [SerializeField] int maxy = 6;
+
+  bool isUpPressed = false;
+  bool isDownPressed = false;
+  bool isLeftPressed = false;
+  bool isRightPressed = false;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    if (Input.GetAxisRaw("Horizontal") == 0)
+    {
+      isLeftPressed = false;
+      isRightPressed = false;
+    }
+
+    if (Input.GetAxisRaw("Vertical") == 0)
+    {
+      isUpPressed = false;
+      isDownPressed = false;
+    }
+
+    if (Input.GetAxisRaw("Horizontal") == -1 && !isLeftPressed)
+    {
+      isLeftPressed = true;
+      if (transform.position.x - 1 < minx) return;
+      transform.position += new Vector3(-1, 0, 0);
+    }
+
+    if (Input.GetAxisRaw("Horizontal") == 1 && !isRightPressed)
+    {
+      isRightPressed = true;
+      if (transform.position.x + 1 > maxx) return;
+      transform.position += new Vector3(1, 0, 0);
+    }
+
+    if (Input.GetAxisRaw("Vertical") == -1 && !isDownPressed)
+    {
+      isDownPressed = true;
+      if (transform.position.y - 1 < miny) return;
+      transform.position += new Vector3(0, -1, 0);
+    }
+
+    if (Input.GetAxisRaw("Vertical") == 1 && !isUpPressed)
+    {
+      isUpPressed = true;
+      if (transform.position.y + 1 > maxy) return;
+      transform.position += new Vector3(0, 1, 0);
+    }
+  }
+
+}
